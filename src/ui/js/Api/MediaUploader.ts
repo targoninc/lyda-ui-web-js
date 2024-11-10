3fa84811ef45f38e4aab8671eecd145e2f46ed31
loudar <alex@targoninc.com>
2024-11-10 02:50:04 +0100

    work on media uploader

--- src/ui/js/Api/MediaUploader.ts
+++ src/ui/js/Api/MediaUploader.ts
@@ -1,21 +1,25 @@
-import {MediaFileType} from "../Enums/MediaFileType.ts";
-import {Api} from "./Api.ts";
-import {ApiRoutes} from "./ApiRoutes.ts";
+import { MediaFileType } from "../Enums/MediaFileType.ts";
+import { ApiRoutes } from "./ApiRoutes.ts";
 
 export class MediaUploader {
     static async upload(type: MediaFileType, referenceId: number, file: File) {
         const formData = new FormData();
         formData.append("type", type);
         formData.append("referenceId", referenceId.toString());
-
         const buffer = await file.arrayBuffer();
         const blob = new Blob([buffer], { type: file.type });
         formData.append("file", blob);
 
-        return await Api.postRawAsync(ApiRoutes.uploadMedia, {
+        const response = await fetch(ApiRoutes.uploadMedia, {
             method: "POST",
             body: formData,
             credentials: "include"
         });
+
+        if (!response.ok) {
+            throw new Error(`Failed to upload media: ${response.statusText}`);
+        }
+
+        return await response.json();
     }
 }
