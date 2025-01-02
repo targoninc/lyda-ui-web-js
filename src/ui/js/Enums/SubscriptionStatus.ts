7d9b7afd519912f6034d00585292885844d989e5
loudar <Alexander Fritsch>
2025-01-02 14:17:41 +0100

    update subscription status

---
+++ src/ui/js/Enums/SubscriptionStatus.ts
@@ -0,0 +1,6 @@
+export enum SubscriptionStatus {
+    created = 0,
+    pending = 1,
+    active = 2,
+    cancelled = 3,
+}
--- src/ui/js/Models/DbModels/finance/Subscription.ts
+++ src/ui/js/Models/DbModels/finance/Subscription.ts
@@ -1,13 +1,14 @@
+import {SubscriptionStatus} from "../../../Enums/SubscriptionStatus.ts";
+
 export interface Subscription {
     id: number;
     user_id: number;
     subscription_id: number;
-    active: boolean;
+    status: SubscriptionStatus;
     billed: number;
     paid: number;
     created_at: Date;
     updated_at: Date;
-    cancelled: boolean;
     cancelled_at: Date;
     previous_subscription: number;
     external_subscription_id: string;
