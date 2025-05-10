6b8fe49e32b8f272209e983df89dcbaa6da9631e
alexa <35202909+loudar@users.noreply.github.com>
2025-05-10 15:54:22 +0200

    fix

--- src/ui/js/Templates/money/PayoutTemplates.ts
+++ src/ui/js/Templates/money/PayoutTemplates.ts
@@ -44,7 +44,7 @@ export class PayoutTemplates {
                     .classes("flex-v", p.status !== PaymentStatus.failed ? "positive" : "negative")
                     .children(
                         create("span")
-                            .text(currency(p.amount_ct * 100, "USD"))
+                            .text(currency(p.amount_ct, "USD"))
                             .build(),
                     ).build(),
                 create("span")
