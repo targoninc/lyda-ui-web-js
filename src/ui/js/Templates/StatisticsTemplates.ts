b5ab64b7f63c2bbad26e2c17a3c8163d38946c0a
alexa <35202909+loudar@users.noreply.github.com>
2025-05-17 21:05:44 +0200

    fixes

--- src/ui/js/Templates/StatisticsTemplates.ts
+++ src/ui/js/Templates/StatisticsTemplates.ts
@@ -1,74 +1,11 @@
-import {Icons} from "../Enums/Icons.ts";
-import {TrackActions} from "../Actions/TrackActions.ts";
 import {UserTemplates} from "./account/UserTemplates.ts";
-import {AlbumActions} from "../Actions/AlbumActions.ts";
-import {PlaylistActions} from "../Actions/PlaylistActions.ts";
 import {Images} from "../Enums/Images.ts";
 import {Util} from "../Classes/Util.ts";
-import {compute, Signal, signal, create, when, nullElement, StringOrSignal, isSignal, asSignal} from "@targoninc/jess";
-import {GenericTemplates} from "./generic/GenericTemplates.ts";
+import {signal, create, when, nullElement} from "@targoninc/jess";
 import { icon } from "@targoninc/jess-components";
-import { EntityType } from "@targoninc/lyda-shared/src/Enums/EntityType";
 import {UserWidgetContext} from "../Enums/UserWidgetContext.ts";
 
 export class StatisticsTemplates {
-    static likesIndicator(type: EntityType, reference_id: number, like_count: number, liked: boolean|Signal<boolean>) {
-        const functionMap: Record<EntityType, Function> = {
-            [EntityType.track]: TrackActions.toggleLike,
-            [EntityType.album]: AlbumActions.toggleLike,
-            [EntityType.playlist]: PlaylistActions.toggleLike,
-        };
-        liked = liked.constructor === Signal ? liked : signal(liked as boolean);
-        const toggleClass = compute((l): string => l ? "enabled" : "_", liked);
-        const imageState = compute(l => l ? Icons.LIKE : Icons.LIKE_OUTLINE, liked);
-
-        return StatisticsTemplates.toggleIndicator(liked, like_count, imageState, reference_id, functionMap[type], [toggleClass]);
-    }
-
-    static repostIndicator(reference_id: number, repost_count: number, reposted: boolean|Signal<boolean>, disabled: Signal<boolean> = signal(false)) {
-        reposted = reposted.constructor === Signal ? reposted : signal(reposted as boolean);
-        const toggleClass = compute((r): string => r ? "enabled" : "_", reposted);
-        const disabledClass = compute((d): string => d ? "disabled" : "_", disabled);
-
-        return StatisticsTemplates.toggleIndicator(reposted, repost_count, Icons.REPOST, reference_id, TrackActions.toggleRepost, [toggleClass, disabledClass]);
-    }
-
-    static toggleIndicator(toggleObservable: Signal<boolean>, count: number, icon: StringOrSignal, reference_id = -1, clickFunc: Function = () => {}, extraClasses: StringOrSignal[] = []) {
-        const count$ = signal(count);
-        toggleObservable.onUpdate = (value: boolean) => {
-            if (!Util.isLoggedIn()) {
-                return;
-            }
-            if (value) {
-                count$.value = count$.value + 1;
-            } else {
-                count$.value = count$.value - 1;
-            }
-        };
-
-        const iconIsUrl = (isSignal(icon) && asSignal(icon).value.startsWith("http")) || (icon as string).startsWith("http");
-        const stateClass = compute((s: boolean): string => s ? "active" : "_", toggleObservable);
-        const stateClass2 = compute((s: boolean): string => s ? "positive" : "_", toggleObservable);
-
-        return create("div")
-            .classes("flex", "stats-indicator")
-            .children(
-                GenericTemplates.roundIconButton({
-                    icon: icon,
-                    adaptive: true,
-                    isUrl: iconIsUrl,
-                }, async () => {
-                    const targetValue = !toggleObservable.value;
-                    toggleObservable.value = targetValue;
-                    clickFunc(reference_id, !targetValue).then((success: boolean) => {
-                        if (!success) {
-                            toggleObservable.value = !targetValue;
-                        }
-                    });
-                }, "", ["stats-indicator-action", stateClass2, stateClass, ...extraClasses]),
-            ).build();
-    }
-
     static genericUserListOpener(type: string, items: any[]) {
         const open$ = signal(false);
         open$.subscribe((value: boolean) => {
