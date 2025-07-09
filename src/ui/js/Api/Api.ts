50c4e1be856a1da2962c841d6f7625f110a0b9ea
alexa <35202909+loudar@users.noreply.github.com>
2025-07-09 21:17:27 +0200

    actually return data...

--- src/ui/js/Api/Api.ts
+++ src/ui/js/Api/Api.ts
@@ -178,7 +178,7 @@ export class Api {
 
     //region Auth
     static async login(email: string, password: string, challenge: string | undefined) {
-        await post<{ user: User } | null>(ApiRoutes.login, {
+        return post<{ user: User } | null>(ApiRoutes.login, {
             email,
             password,
             challenge,
