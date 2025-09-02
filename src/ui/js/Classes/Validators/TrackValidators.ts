452149fd9869d16e5c663adffcaf4109d8de9ca8
alexa <alex@targoninc.com>
2025-09-02 19:15:22 +0200

    stricter validation

--- src/ui/js/Classes/Validators/TrackValidators.ts
+++ src/ui/js/Classes/Validators/TrackValidators.ts
@@ -5,6 +5,6 @@ export class TrackValidators {
     static artistnameValidators = [maxLengthValidator(128)];
     static titleValidators = [maxLengthValidator(255)];
     static isrcValidators = [exactLengthValidator(12, true)];
-    static upcValidators = [minLengthValidator(12, true)];
+    static upcValidators = [minLengthValidator(12, true), maxLengthValidator(16)];
     static descriptionValidators = [maxLengthValidator(2048)];
 }
