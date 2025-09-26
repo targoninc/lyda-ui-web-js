8cca2bd0c8b837d5e695a05dde33995b8b96e7f5
Alexander Fritsch <alex@targoninc.com>
2025-09-26 13:11:29 +0200

    even more smarter sorting

--- src/ui/js/Classes/Helpers/Sorting.ts
+++ src/ui/js/Classes/Helpers/Sorting.ts
@@ -10,6 +10,16 @@ export function sortByProperty<T>(c: keyof T | null, l: T[]) {
             return aProp - bProp;
         }
 
+        if (typeof aProp === "object" && typeof bProp === "object") {
+            if (aProp instanceof Object && bProp instanceof Object) {
+                return JSON.stringify(aProp).localeCompare(JSON.stringify(bProp));
+            }
+
+            if (aProp instanceof Array && bProp instanceof Array) {
+                return aProp.length - bProp.length;
+            }
+        }
+
         const timeIndicators = ["time", "_at"];
         if (timeIndicators.some(ti => (c as string).includes(ti))) {
             return new Date(bProp as string).getTime() - new Date(aProp as string).getTime();
