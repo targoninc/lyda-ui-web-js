6a47da2deaf95808063e914ca26f7e5e9e62993d
alexa <alex@targoninc.com>
2025-10-05 19:00:17 +0200

    fix a bunch of feed stuff

--- src/ui/js/Templates/music/MusicTemplates.ts
+++ src/ui/js/Templates/music/MusicTemplates.ts
@@ -303,7 +303,7 @@ export class MusicTemplates {
         const empty = compute((t, l) => t.length === 0 && !l, entities$, loading$);
 
         return create("div")
-            .classes("flex-v", "fullHeight", "fullWidth")
+            .classes("flex-v", "fullWidth")
             .children(
                 horizontal(
                     horizontal(
