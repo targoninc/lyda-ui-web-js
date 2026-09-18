94f86266b36b1ec58add2fce0fcbf1c18d31f57e
alex <alex@targoninc.com>
2026-09-18 14:16:48 +0200

    don't track errors for non logged-in users

--- src/ui/js/Classes/Helpers/ErrorHandler.ts
+++ src/ui/js/Classes/Helpers/ErrorHandler.ts
@@ -1,7 +1,12 @@
 import { ClientError } from "@targoninc/lyda-shared/src/Models/db/lyda/ClientError";
 import { Api } from "../../Api/Api.ts";
+import { currentUser } from "../../state.ts";
 
 async function handleError(msg: string, url: string, line: number, col: number, error: Error) {
+    if (!currentUser.value) {
+        return;
+    }
+
     await Api.trackClientError(<ClientError>{
         message: msg,
         url: url,
